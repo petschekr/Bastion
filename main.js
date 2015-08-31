2ea274aac50d612ea75f993a93aea2013c9d6d22
Ryan Petschek <petschekr@gmail.com>
2015-08-30 21:24:38 -0400

    Switch to native file dialogs called from main.js

--- main.js
+++ main.js
@@ -11,8 +11,35 @@ app.on("window-all-closed", function() {
 	}
 });
 
-ipc.on("database", function(event) {
+ipc.on("database", function (event) {
 	event.returnValue = app.getPath("userData");
+});
+ipc.on("open", function (event, data) {
+	dialog.showOpenDialog(mainWindow, {
+		"title": data.title || "Open",
+		"defaultPath": app.getPath("home"),
+		"filters": data.filters,
+		"properties": [
+			"openFile"
+		]
+	}, function (file) {
+		// file === null if the dialog was canceled
+		if (!file)
+			return;
+		event.sender.send("open-response", file);
+	});
+});
+ipc.on("save", function (event, data) {
+	dialog.showSaveDialog(mainWindow, {
+		"title": data.title || "Open",
+		"defaultPath": app.getPath("home"),
+		"filters": data.filters
+	}, function (file) {
+		// file === null if the dialog was canceled
+		if (!file)
+			return;
+		event.sender.send("save-response", file);
+	});
 });
 
 app.on("ready", function() {
